b4530535d83209a0177c4b39bff19c653e71adf8
bCubed3 <sf.bcubed@gmail.com>
2022-05-27 22:07:08 -0700

    Toilet Flush
    The toilet now flushes. The flush button could use an animation and the text is now showing up for some reason.

--- mod.lua
+++ mod.lua
@@ -15,3 +15,4 @@ function init()
 
     return "Success"
 end
+
--- modules/toilet.lua
+++ modules/toilet.lua
@@ -7,10 +7,43 @@ function define_toilet()
         shop_key = true,
         shop_buy = 0,
         shop_sell = 0,
-        layout = {},
+        layout = {
+            {7, 17},
+            {30, 17},
+            {53, 17},
+            {7, 40},
+            {30, 40},
+            {53, 40}
+        },
         buttons = {"Help", "Move", "Target", "Close"},
         info = {},
         tools = {"mouse1", "hammer1"},
         placeable = true
-        }, "sprites/toilet/toilet.png", "sprites/toilet/toilet_gui.png", {})
+        }, "sprites/toilet/toilet.png", "sprites/toilet/toilet_gui.png", {
+            define = "define_flush_button",
+            draw = "draw_toilet_gui"
+        })
+end
+
+function define_flush_button(menu_id)
+    --api_dp(menu_id, "flush_button_sprite", api_define_sprite("flush_button", "sprites/toilet/flush_button.png"))
+    api_define_button(
+        menu_id,
+        "flush_button",
+        5,
+        60,
+        "Flush!",
+        "flush_toilet",
+        "sprites/toilet/flush_button.png"
+    )
+end
+
+function flush_toilet(menu_id)
+    for i=1,6 do
+        api_slot_clear(i)
+    end
+end
+
+function draw_toilet_gui(menu_id)
+    api_draw_button(api_gp(menu_id, "flush_button"))
 end
