5b8d2a2de8de57414b6841711f5aaf9756870ed2
bCubed3 <sf.bcubed@gmail.com>
2022-06-01 22:48:35 -0700

    Added recipe selection and number drawing
    Still missing tabs

--- modules/crafting.lua
+++ modules/crafting.lua
@@ -1,4 +1,5 @@
 DECO_WORKBENCH_RECIPES = {}
+DECO_WORKBENCH_RECIPE_INDEX = {}
 RECIPE_SLOT_AMOUNT = 5
 SLOT_SPR = -1
 RECIPE_SPR = -1
@@ -10,6 +11,8 @@ TAB_2_SPR = -1
 TAB_2_SELECTED_SPR = -1
 CAN_CRAFT_SPR = -1
 CANT_CRAFT_SPR = -1
+WHITE_NUMBERS_SPR = -1
+RED_NUMBERS_SPR = -1
 SPR_REF = {}
 
 -- defines recipes (used by other scripts)
@@ -21,6 +24,7 @@ dw_define_recipe({{"item1", 1}, {"item2", 3}}, "output_item", 3)
 ]]--
 function dw_define_recipe(input_table, output, num)
 	table.insert(DECO_WORKBENCH_RECIPES, {input_table, {output, num}})
+	DECO_WORKBENCH_RECIPE_INDEX[output] = #DECO_WORKBENCH_RECIPES
 	for i=1,#input_table do
 		SPR_REF[input_table[i][1]] = api_get_sprite("sp_" .. input_table[i][1])
 	end
@@ -38,6 +42,8 @@ function prep_deco_workbench()
 	TAB_1_SELECTED_SPR = api_define_sprite("dw_tab_1_s", "sprites/crafting/dw_tab_1_selected.png", 2)
 	TAB_2_SPR = api_define_sprite("dw_tab_2", "sprites/crafting/dw_tab_2.png", 2)
 	TAB_2_SELECTED_SPR = api_define_sprite("dw_tab_2_s", "sprites/crafting/dw_tab_2_selected.png", 2)
+	WHITE_NUMBERS_SPR = api_define_sprite("white_numbers_sprite", "sprites/crafting/white_numbers.png", 13)
+	RED_NUMBERS_SPR = api_define_sprite("red_numbers_sprite", "sprites/crafting/red_numbers.png", 13)
 end
 
 function define_deco_workbench()
@@ -57,8 +63,8 @@ function define_deco_workbench()
     	placeable = true
     }, "sprites/crafting/decoration_workbench.png", "sprites/crafting/decoration_workbench_gui.png", {
         define = "deco_workbench_define",
-		draw = "deco_workbench_draw",
-		tick = "deco_workbench_tick"
+		draw = "deco_workbench_draw"
+		--tick = "deco_workbench_tick"
     }, nil)
 end
 
@@ -95,7 +101,6 @@ function deco_workbench_draw(menu_id)
 	api_draw_button(api_gp(menu_id, "decrease_results"), false)
 	api_draw_button(api_gp(menu_id, "increase_results"), false)
 	api_draw_button(api_gp(menu_id, "craft_amount_display"), true)
-	api_log("camt", "draw !")
 
 	-- the rest of this function draws the sprites to choose recipes
 	for i=1,#DECO_WORKBENCH_RECIPES do
@@ -108,40 +113,58 @@ function deco_workbench_draw(menu_id)
 	if api_gp(menu_id, "selected_item") ~= nil then
 		recipe = api_gp(menu_id, "selected_recipe")
 		recipe_length = #recipe[1]
-		ingredient1 = recipe[1][1][1]
+		ingredient1 = recipe[1][1]
 		ingredient2 = nil
 		ingredient3 = nil
 		output = recipe[2][1]
 		offset = 0
 		ox = api_gp(menu_id, "x") - cam["x"]
 		oy = api_gp(menu_id, "y") - cam["y"]
+		inv_amts = check_inventory(menu_id)
+		can_craft_slots = inv_amts[2]
+		inv_amts = inv_amts[1]
+		amt = api_gp(menu_id, "craft_amount")
+		can_craft_color = {}
+		for i=1,#can_craft_slots do
+			if can_craft_slots[i] == false then
+				can_craft_color[i] = "RED"
+			elseif can_craft_slots[i] == true then
+				can_craft_color[i] = "WHITE"
+			end
+		end
 		if recipe_length == 1 then
 			-- draw middle sprite
 			api_draw_sprite(SLOT_SPR, 0, ox + 158, oy + 20)
-			api_draw_sprite(SPR_REF[ingredient1], 0, ox + 160, oy + 22)
+			api_draw_sprite(SPR_REF[ingredient1[1]], 0, ox + 160, oy + 22)
+			draw_numbers(ox + 158, oy + 37, tostring(inv_amts[1]), tostring(ingredient1[2] * amt), can_craft_color[1])
 		elseif recipe_length == 2 then
-			ingredient2 = recipe[1][2][1]
+			ingredient2 = recipe[1][2]
 			-- draw the left sprite
 			api_draw_sprite(SLOT_SPR, 0, ox + 158 - 19, oy + 20)
-			api_draw_sprite(SPR_REF[ingredient1], 0, ox + 160 - 19, oy + 22)
+			api_draw_sprite(SPR_REF[ingredient1[1]], 0, ox + 160 - 19, oy + 22)
+			draw_numbers(ox + 158 - 19, oy + 37, tostring(inv_amts[1]), tostring(ingredient1[2] * amt), can_craft_color[1])
 			-- draw the right sprite
 			api_draw_sprite(SLOT_SPR, 0, ox + 158 + 19, oy + 20)
-			api_draw_sprite(SPR_REF[ingredient2], 0, ox + 160 + 19, oy + 22)
+			api_draw_sprite(SPR_REF[ingredient2[1]], 0, ox + 160 + 19, oy + 22)
+			draw_numbers(ox + 158 + 19, oy + 37, tostring(inv_amts[2]), tostring(ingredient2[2]), can_craft_color[2])
 
 			-- draw the plus
 			api_draw_sprite(PLUS_SPR, 0, ox + 166, oy + 28)
 		else
-			ingredient2 = recipe[1][2][1]
-			ingredient3 = recipe[1][3][1]
+			ingredient2 = recipe[1][2]
+			ingredient3 = recipe[1][3]
 			-- draw the left sprite
 			api_draw_sprite(SLOT_SPR, 0, ox + 158 - 39, oy + 20)
-			api_draw_sprite(SPR_REF[ingredient1], 0, ox + 160 - 39, oy + 22)
+			api_draw_sprite(SPR_REF[ingredient1[1]], 0, ox + 160 - 39, oy + 22)
+			draw_numbers(ox + 158 - 39, oy + 37, tostring(inv_amts[1]), tostring(ingredient1[2] * amt), can_craft_color[1])
 			-- draw middle sprite
 			api_draw_sprite(SLOT_SPR, 0, ox + 158, oy + 20)
-			api_draw_sprite(SPR_REF[ingredient2], 0, ox + 160, oy + 22)
+			api_draw_sprite(SPR_REF[ingredient2[1]], 0, ox + 160, oy + 22)
+			draw_numbers(ox + 158, oy + 37, tostring(inv_amts[2]), tostring(ingredient2[2]), can_craft_color[2])
 			-- draw the right sprite
 			api_draw_sprite(SLOT_SPR, 0, ox + 158 + 39, oy + 20)
-			api_draw_sprite(SPR_REF[ingredient3], 0, ox + 160 + 39, oy + 22)
+			api_draw_sprite(SPR_REF[ingredient3[1]], 0, ox + 160 + 39, oy + 22)
+			draw_numbers(ox + 158 + 39, oy + 37, tostring(inv_amts[3]), tostring(ingredient3[2]), can_craft_color[3])
 
 			-- draw the pluses
 			api_draw_sprite(PLUS_SPR, 0, ox + 146, oy + 28)
@@ -152,29 +175,39 @@ function deco_workbench_draw(menu_id)
 		-- draw recipe output
 		api_draw_sprite(SLOT_SPR, 1, ox + 158, oy + 57)
 		api_draw_sprite(SPR_REF[output], 0, ox + 160, oy + 59)
+		draw_number(ox + 158 + 21, oy + 59 + 15, tostring(amt), "WHITE", "RIGHT")
 
 		-- draw craft button
 		api_draw_button(api_gp(menu_id, "craft_button"), true)
 	end
 end
 
-function deco_workbench_tick(menu_id)
+function check_inventory(menu_id)
 	if api_gp(menu_id, "open") == true then
-		recipe = api_gp(menu_id, "selected_recipe")
-		recipe_length = #recipe[1]
-		recipe_multi = api_gp(menu_id, "craft_amount")
-		recipe[1][1][2] = recipe[1][1][2] * recipe_multi
-		recipe[2][2] = recipe[2][2] * recipe_multi
 		-- check that the player + other menus have enough items
+		recipe = api_gp(menu_id, "selected_recipe")
 		if recipe ~= nil then
-			can_craft = api_use_total(recipe[1][1][1]) >= recipe[1][1][2]
+			recipe_length = #recipe[1]
+			recipe_multi = api_gp(menu_id, "craft_amount")
+			recipe[1][1][2] = recipe[1][1][2] * recipe_multi
+			recipe[2][2] = recipe[2][2] * recipe_multi
+			i1amt = api_use_total(recipe[1][1][1])
+			out = {i1amt}
+			can_craft = i1amt >= recipe[1][1][2]
+			can_craft_out = {can_craft}
 			if recipe_length >= 2 then
+				i2amt = api_use_total(recipe[1][2][1])
+				out[2] = i2amt
 				recipe[1][2][2] = recipe[1][2][2] * recipe_multi
-				can_craft = can_craft and api_use_total(recipe[1][2][1]) >= recipe[1][2][2]
+				can_craft_out[2] = i2amt >= recipe[1][2][2]
+				can_craft = can_craft and can_craft_out[2]
 			end
 			if recipe_length >= 3 then
+				i3amt = api_use_total(recipe[1][3][1])
+				out[3] = i3amt
 				recipe[1][3][2] = recipe[1][3][2] * recipe_multi
-				can_craft = can_craft and api_use_total(recipe[1][3][1]) >= recipe[1][3][2]
+				can_craft_out[3] = i3amt >= recipe[1][3][2]
+				can_craft = can_craft and can_craft_out[3]
 			end
 
 			-- set the crafting button to the proper sprite
@@ -185,6 +218,7 @@ function deco_workbench_tick(menu_id)
 				api_sp(menu_id, "invalid", true)
 				api_sp(api_gp(menu_id, "craft_button"), "sprite_index", CANT_CRAFT_SPR)
 			end
+			return {out, can_craft_out}
 		end
 	end
 end
@@ -213,7 +247,10 @@ function dw_craft_click(menu_id)
 end
 
 function dw_recipe_click(menu_id)
-	
+	button_id = api_get_highlighted("ui")
+	api_sp(menu_id, "selected_item", api_gp(button_id, "text"))
+	api_sp(menu_id, "selected_recipe", DECO_WORKBENCH_RECIPES[DECO_WORKBENCH_RECIPE_INDEX[api_gp(button_id, "text")]])
+
 end
 
 function dw_tab_click(menu_id)
@@ -235,7 +272,6 @@ function dw_decrease_results(menu_id)
 end
 
 function update_amount_display(menu_id, amt)
-	api_log("amt", amt)
 	api_sp(api_gp(menu_id, "craft_amount_display"), "text", amt)
 end
 
@@ -264,4 +300,54 @@ function dw_do_nothing(menu_id)
 	-- if you've made it this far, good job, and i apologize for my code
 	-- good luck !
 	-- this function does nothing
+end
+
+function draw_numbers(x, y, n1, n2, color, justify)
+	justify = "CENTER" or justify
+	offset = 4
+	start = 0
+	if justify == "CENTER" then
+		start = -((#n1 + #n2 - 3) * offset / 2) + 10
+	elseif justify == "RIGHT" then
+		start = -((#n1 + #n2 - 3) * offset)
+	end
+	drawn = 0
+	if color == "WHITE" then 
+		numbers = WHITE_NUMBERS_SPR
+	else
+		numbers = RED_NUMBERS_SPR
+	end
+	-- draw n1 (limited to three chars)
+	for i=1, #n1 - 2 do
+		api_draw_sprite(numbers, tonumber(string.sub(n1, i, i)), x + drawn * offset + start, y)
+		drawn = drawn + 1
+	end
+	api_draw_sprite(numbers, 11, x + drawn * offset + start, y)
+	drawn = drawn + 1
+	for i=1, #n2 - 2 do
+		api_draw_sprite(numbers, tonumber(string.sub(n2, i, i)), x + drawn * offset + start, y)
+		drawn = drawn + 1
+	end
+end
+
+function draw_number(x, y, n1, color, justify)
+	justify = justify or "CENTER"
+	offset = 4
+	start = 0
+	if justify == "CENTER" then
+		start = -((#n1 - 2) * offset / 2) + 10
+	elseif justify == "RIGHT" then
+		start = -((#n1 - 2) * offset)
+	end
+	drawn = 0
+	if color == "WHITE" then 
+		numbers = WHITE_NUMBERS_SPR
+	else
+		numbers = RED_NUMBERS_SPR
+	end
+	-- draw n1 (limited to three chars)
+	for i=1, #n1 - 2 do
+		api_draw_sprite(numbers, tonumber(string.sub(n1, i, i)), x + drawn * offset + start, y)
+		drawn = drawn + 1
+	end
 end
